a6f2f1a216494c174bbc948d4779f4756170ac12
LeoLombardi <leonardobotinelly+github@gmail.com>
2016-05-17 09:16:39 -0400

    Fixed .gitignore

--- b.bat
+++ b.bat
@@ -1,2 +1,4 @@
 RMDIR "tos-laimas-compass-win32-x64" /S /Q
 electron-packager . --platform=win32 --arch=x64 --overwrite
+DEL "tos-laimas-compass-win32-x64\resources\app\.gitignore" /a:H
+DEL "tos-laimas-compass-win32-x64\resources\app\*.bat"
--- tos-laimas-compass-win32-x64/resources/app/b.bat
+++
@@ -1,2 +0,0 @@
-RMDIR "tos-laimas-compass-win32-x64" /S /Q
-electron-packager . --platform=win32 --arch=x64 --overwrite
