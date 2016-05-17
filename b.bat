70f6342beea8bf18a85dbf1dd001ee7001abb5c1
LeoLombardi <leonardobotinelly+github@gmail.com>
2016-05-17 13:14:58 -0400

    Clutter B-gone

--- b.bat
+++ b.bat
@@ -1,4 +1,23 @@
 RMDIR "tos-laimas-compass-win32-x64" /S /Q
-electron-packager . --platform=win32 --arch=x64 --overwrite
-DEL "tos-laimas-compass-win32-x64\resources\app\.gitignore" /a:H
-DEL "tos-laimas-compass-win32-x64\resources\app\*.bat"
+call electron-packager . --platform=win32 --arch=x64 --overwrite
+CD
+DEL ".\tos-laimas-compass-win32-x64\resources\app\.gitignore" /f
+DEL ".\tos-laimas-compass-win32-x64\resources\app\*.bat" /f
+
+DEL ".\tos-laimas-compass-win32-x64\resources\app\*.less" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\.npmignore" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\.dockerignore" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\makefile" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\.tern-port" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\.grunt.js" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\.LICENSE-MIT" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\.travis.yml" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\*.md" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\LICENSE" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\AUTHORS" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\*.gzip" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\*.map" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\*.iml" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\*.ipr" /f /s
+DEL ".\tos-laimas-compass-win32-x64\resources\app\*.iws" /f /s
+
